63c0ee08b236cfe87b779fe031a75b06a2852c8a
Sudip Paul <146758688+Sudip-2@users.noreply.github.com>
2025-05-04 19:34:22 +0530

    mades some upgrades

--- auth-in-nextjs/src/app/api/users/login/route.ts
+++ auth-in-nextjs/src/app/api/users/login/route.ts
@@ -30,7 +30,7 @@ export async function POST(req: NextRequest) {
       expiresIn: "1d",
     });
     const response = NextResponse.json({ message: "Login Successfull" });
-    response.cookies.set("token", token, { httpOnly: true });
+    response.cookies.set("token", token, { httpOnly: true ,expires:new Date(Date.now() + 24 * 60 * 60 * 1000)});
     return response;
   } catch (err: any) {
     return NextResponse.json({ error: err.message }, { status: 500 });
